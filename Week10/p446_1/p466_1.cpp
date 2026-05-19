e35bcdbaae8e1f5388e17a657bcee7bc060c8feb
ho21360 <lee021104github.com>
2026-05-19 11:51:13 +0900

    Week10

---
+++ Week10/p446_1/p466_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Point {
+protected:
+    int x, y;
+public:
+    Point(int xx, int yy) : x(xx), y(yy) {}
+    void draw() { std::cout << x << ", " << y << "에 점을 그리라.\n"; }
+};
+
+class Rectangle1 : public Point { // (1) 상속
+private:
+    int width, height;            // 멤버 변수 추가
+
+public:
+    // (2) 멤버 초기화 리스트
+    Rectangle1(int xx, int yy, int w, int h) : Point(xx, yy), width(w), height(h) {}
+
+    // (3) 함수 재정의
+    void draw() {
+        std::cout << "\"" << x << ", " << y << "에 가로 " << width 
+                  << " 세로 " << height << "인 사각형을 그리라\"\n";
+    }
+};
+
+int main() {
+    Rectangle1 rect(2, 3, 100, 200);
+    rect.draw(); // (3)번 예시 출력용
+    return 0;
+}
